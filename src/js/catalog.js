f5035c77176da70dfe081fd85f8cbc14a2f3327f
Daniele Salvatore <daniele.salvatore@fao.org>
2016-06-10 11:18:04 +0200

    FENIX components options harmonization

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -98,16 +98,16 @@ define([
 
     /**
      * pub/sub
-     * @return {Object} catalog instance
+     * @return {Object} component instance
      */
-    Catalog.prototype.on = function (channel, fn) {
+    Catalog.prototype.on = function (channel, fn, context) {
+        var _context = context || this;
         if (!this.channels[channel]) {
             this.channels[channel] = [];
         }
-        this.channels[channel].push({context: this, callback: fn});
+        this.channels[channel].push({context: _context, callback: fn});
         return this;
     };
-
     /**
      * Dispose
      * @return {null}
